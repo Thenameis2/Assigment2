72131ec901f9c62231a8733364ba96344ff6d536
Mihiretu Jackson <mihiretujackson@gmail.com>
2025-04-10 21:17:43 -0400

    chore: declared  functions for dynamiclinkedlist

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,69 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+use std::fmt::Debug;
+
+pub struct Node<T> 
+where
+    T: PartialEq + Debug + Clone,
+{
+    data: T,
+    next: Option<Box<Node<T>>>,
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+pub struct DynamicLinkedList<T> 
+where
+    T: PartialEq + Debug + Clone,
+{
+    head: Option<Box<Node<T>>>,
+    size: usize,
+}
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+impl<T> DynamicLinkedList<T> 
+where
+    T: PartialEq + Debug + Clone,
+{
+    pub fn new() -> Self {
+        DynamicLinkedList {
+            head: None,
+            size: 0,
+        }
+    }
+    
+    pub fn insert(&mut self, data: T) {
+        todo!("Not Implemented");
+    }
+
+    pub fn insert_at_index(&mut self, index: usize, data: T) {
+        todo!("Not Implemented");
+    }
+
+    pub fn delete_element(&mut self, data: T) -> bool {
+        todo!("Not Implemented");
+    }
+
+    pub fn delete_at_index(&mut self, index: usize) -> bool {
+        todo!("Not Implemented");
+    }
+
+    pub fn update_element(&mut self, old_data: T, new_data: T) -> bool {
+        todo!("Not Implemented");
+    }
+
+    pub fn update_element_at_index(&mut self, index: usize, data: T) -> bool {
+        todo!("Not Implemented");
+    }
+
+    pub fn find(&self, data: T) -> bool {
+        todo!("Not Implemented");
+    }
+
+    pub fn get(&self, index: usize) -> Option<T> {
+        todo!("Not Implemented");
+    }
+    
+    pub fn len(&self) -> usize {
+        self.size
+    }
+    
+    pub fn is_empty(&self) -> bool {
+        self.size == 0
     }
 }
